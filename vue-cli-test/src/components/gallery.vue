032d544e3f189656053021a039a438d67824c0a1
Taiga Ishii <taigaishii@TaiganoMacBook-Pro.local>
2018-01-06 12:18:11 +0900

    add-gallery-json

--- vue-cli-test/src/components/gallery.vue
+++ vue-cli-test/src/components/gallery.vue
@@ -1,37 +1,76 @@
 <template>
   <section>
-    <h1>My first gellery</h1>
+    <!--一番上のリンクバー-->
+    <header>
+      <nav>
+        <ul>
+          <li><router-link to="/" class="nav-link">Home</router-link></li>
+          <li><router-link to="/gallery" class="nav-link">Gallaery</router-link></li>
+        </ul>
+      </nav>
+    </header>
+    <!--ランダムで画像を表示する-->
+    <h1>Gallery</h1>
+      <div v-for="(List, anim) in this.$store.gallery.list">
+        <div v-for="img in list">
+          <img v-bind:src="imgUrl('./' + anim + '/' + img)">
+        </div>
+      </div>
   </section>
 </template>
 
 <script>
+const images = require.context('..assets/img', true, /\.jpg$/);
+
 export default {
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
     };
+  },
+  methods:{
+    imgUrl(path){
+      return images(path);
+    },
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  section {
-    color:white;
-    height: 100vh;
-    width: 100vw;
-    background-image: url(../assets/screen_example.jpg);
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: cover;
+header {
+    background: #000;
+    height: 45px;
+    position: relative;
   }
-  h1{
-    font-weight: normal;
-    margin: 0;
+
+  nav {
     position: absolute;
+    width: 100%;
     top: 50%;
     left: 50%;
-    -webkit-transform: translateY(-50%) translateX(-50%);
     transform: translateY(-50%) translateX(-50%);
+  }
+
+  ul {
+    list-style: none;
+    -webkit-flex-direction: row;
+    flex-direction: row;
+    display: -webkit-flex;
+    display: flex;
+    -webkit-justify-content: space-around;
+    justify-content: space-around;
+    padding: 0;
+    width: 300px;
+    margin: auto;
+  }
+
+  .nav-link {
+    color: #fff;
+    text-decoration: none;
+  }
+
+  img {
+    width: 200px;
+    height: 200px;
   }
 </style>
